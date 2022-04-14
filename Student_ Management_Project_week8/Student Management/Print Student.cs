f1d893402a1637d36561a1bd149c527b1c695f25
AlexanderSlokov <102212788+AlexanderSlokov@users.noreply.github.com>
2022-04-14 14:54:43 +0700

    [update] print

---
+++ Student_ Management_Project_week8/Student Management/Print Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student__Management_Project_week8
+{
+    public partial class Print_Student : Form
+    {
+        public Print_Student()
+        {
+            InitializeComponent();
+        }
+    }
+}
