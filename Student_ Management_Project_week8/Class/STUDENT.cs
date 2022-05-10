9f7d8644921511a132c939f8de29861a18ccfa65
AlexanderSlokov <102212788+AlexanderSlokov@users.noreply.github.com>
2022-05-10 21:37:58 +0700

    [10/5]

--- Student_ Management_Project_week8/Class/STUDENT.cs
+++ Student_ Management_Project_week8/Class/STUDENT.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 //SQL command classes based on this 
 using System.IO;
-using System.Drawing;
 using System.Windows.Forms;
 
 
 namespace Student_Management_Project_week8
 {
-     class STUDENT
+    class STUDENT
     {
         MY_DB mydb = new MY_DB();
 
